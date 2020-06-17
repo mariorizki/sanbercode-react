ee011fdb8bc379f266c309a6b10e975a76a3cbab
Mario Rizki <mariorizki07@gmail.com>
2020-06-18 02:29:14 +0700

    done with soal 2

---
+++ week1/tugas3.js
@@ -0,0 +1,12 @@
+// Soal 2
+const objectBebas = {
+  aspal: 'hitam',
+  roket: 'meluncur',
+  joker: 'villain',
+  unta: 'arab',
+  ninja: 'hatori'
+};
+
+const { aspal: a, roket: r, joker: j, unta: u, ninja: n } = objectBebas;
+
+console.log(a, r, j, u, n);
