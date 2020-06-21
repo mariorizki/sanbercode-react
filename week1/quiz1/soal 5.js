553b7f4714eca8d24db31e522887a0e32ded8986
Mario Rizki <mariorizki07@gmail.com>
2020-06-21 15:11:58 +0700

    minor revision

--- week1/quiz1/soal 5.js
+++ week1/quiz1/soal 5.js
@@ -1,7 +1,7 @@
 const reverseWithoutE = str => {
   let result = '';
   for (let i = str.length - 1; i >= 0; i--) {
-    if (str[i] !== 'e') {
+    if (str[i] !== 'e' && str[i] !== 'E') {
       result += str[i];
     }
   }
